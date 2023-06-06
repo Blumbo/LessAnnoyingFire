6e4ddfad3eb1c61829df7e586ecc87b256f215a1
Blumbo <blumboman@gmail.com>
2023-06-06 19:44:18 +0300

    update to 1.19.4

--- src/main/java/net/blumbo/lessannoyingfire/mixin/LivingEntityMixin.java
+++ src/main/java/net/blumbo/lessannoyingfire/mixin/LivingEntityMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.Opcodes;
@@ -17,7 +18,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
 
-    @Shadow public float knockbackVelocity;
     @Shadow @Nullable private DamageSource lastDamageSource;
 
     @Shadow public abstract boolean damage(DamageSource source, float amount);
@@ -36,7 +36,7 @@ public abstract class LivingEntityMixin extends Entity {
     @Redirect(method = "damage", at = @At(value = "FIELD", ordinal = 0, opcode = Opcodes.GETFIELD, target = "Lnet/minecraft/entity/LivingEntity;timeUntilRegen:I"))
     private int uh(LivingEntity value) {
         // Make fire caused invulnerability ticks irrelevant if damage comes from an entity
-        if (fireDamageSource(lastDamageSource) && damageSource.getSource() != null) return 0;
+        if (fireDamageSource(value, lastDamageSource) && damageSource.getSource() != null) return 0;
         return timeUntilRegen;
     }
 
@@ -48,11 +48,13 @@ public abstract class LivingEntityMixin extends Entity {
     @Redirect(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;scheduleVelocityUpdate()V"))
     private void yes(LivingEntity instance) {
         // Prevent fire from messing up movement
-        if (!fireDamageSource(damageSource) || knockbackVelocity != 0.0f) scheduleVelocityUpdate();
+        if (!fireDamageSource(instance, damageSource)) scheduleVelocityUpdate();
+
     }
 
-    private static boolean fireDamageSource(DamageSource damageSource) {
-        return damageSource == DamageSource.ON_FIRE || damageSource == DamageSource.IN_FIRE;
+    private static boolean fireDamageSource(LivingEntity entity, DamageSource damageSource) {
+        DamageSources damageSources = entity.getWorld().getDamageSources();
+        return damageSource == damageSources.onFire() || damageSource == damageSources.inFire();
     }
 
 }
